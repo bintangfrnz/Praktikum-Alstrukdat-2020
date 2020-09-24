3580ca03e34870a0015f01b080b8d9fa6cd366d0
Bintang F <42971243+bintangfrnz@users.noreply.github.com>
2020-09-24 23:38:51 +0700

    Score: 100/100

---
+++ Praktikum 1/Soal 1/hello.c
@@ -0,0 +1,17 @@
+/*
+ * NIM       : 13519138
+ * Nama      : Bintang Fajarianto
+ * Tanggal   : 27 Agustus 2020
+ * Praktikum : 1
+ * Soal		 : 1
+ * Deskripsi : Menampilkan "Hello, World!" ke layar.
+ */
+
+#include <stdio.h>
+
+int main() {
+	/* ALGORITMA */
+	printf("Hello, World!\n");
+
+	return 0;
+}
